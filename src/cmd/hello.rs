16f635faccd07228ec353215084e5a2f25548e86
jzm2k <jzm2k@protonmail.com>
2021-06-15 06:49:05 +0300

    cmd/hello: Add support for SAM HELLO message

---
+++ src/cmd/hello.rs
@@ -0,0 +1,92 @@
+use crate::error::I2pError;
+use crate::socket::I2pSocket;
+use crate::parser::{Command, Subcommand, parse};
+
+static MIN_VERSION: &'static str = "3.1";
+static MAX_VERSION: &'static str = "3.1";
+
+/// Parse and validate router's SAMv3-compatible response
+///
+/// # Arguments
+/// `response` - Router's response in text format
+///
+fn parse_response(response: &str) -> Result<(), I2pError> {
+
+    let parsed = match parse(response, Command::Hello, Some(Subcommand::Reply)) {
+        Ok(v)  => v,
+        Err(e) => {
+            eprintln!("Failed to parse response: {:#?}", e);
+            return Err(I2pError::InvalidValue);
+        }
+    };
+
+    match parsed.get_value("RESULT") {
+        Some(v) => {
+            match &v[..] {
+                "OK" => {
+                    Ok(())
+                },
+                "I2P_ERROR" => {
+                    return Err(I2pError::RouterError);
+                },
+                "NOVERSION" => {
+                    return Err(I2pError::InvalidValue);
+                },
+                _ => {
+                    eprintln!("Unknown response from router: {}", v);
+                    return Err(I2pError::Unknown);
+                }
+            }
+        },
+        None => {
+            eprintln!("Router respones did not contain RESULT!");
+            return Err(I2pError::InvalidValue);
+        }
+    }
+}
+
+/// handshake_internal() sends the specified message to the router and reads a response
+/// with a timeout.
+///
+/// handshake_internal() expects, as the spec requires, that the message the router sends
+/// ends in a newline (\n)
+///
+/// When the message has been read, it's parsed and validated
+///
+/// # Arguments
+///
+/// `socket` - I2pSocket object created by the caller.
+/// `msg` - SAMv3 message that is sent to the router
+///
+fn handshake_internal(socket: &mut I2pSocket, msg: &str) -> Result<(), I2pError> {
+
+    match socket.write(msg.as_bytes()) {
+        Ok(_)  => {},
+        Err(e) => {
+            eprintln!("Failed to send HELLO command to the router: {:#?}", e);
+            return Err(I2pError::TcpStreamError);
+        }
+    }
+
+    let mut data = String::with_capacity(128);
+    match socket.read(&mut data) {
+        Ok(_) => { },
+        Err(e) => {
+            eprintln!("Failed to read response from router: {:#?}", e);
+            return Err(e);
+        }
+    }
+
+    parse_response(&data)
+}
+
+/// Handshake with the router to establish initial connection
+///
+/// # Arguments
+///
+/// `socket` - I2pSocket object created by the caller
+///
+pub fn handshake(socket: &mut I2pSocket) -> Result<(), I2pError> {
+    let msg = format!("HELLO VERSION MIN={} MAX={}\n", MIN_VERSION, MAX_VERSION);
+    handshake_internal(socket, &msg)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod error;
 pub mod socket;
 pub mod parser;
+pub mod cmd;
