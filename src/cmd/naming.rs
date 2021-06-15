dfe3117c97077e79f2f58133a3218b1bc955bdba
jzm2k <jzm2k@protonmail.com>
2021-06-15 09:44:06 +0300

    cmd/naming/tests: Add tests for NAMING

--- src/cmd/naming.rs
+++ src/cmd/naming.rs
@@ -94,3 +94,38 @@ fn lookup_internal(socket: &mut I2pSocket, msg: &str) -> Result<(String, String)
 pub fn lookup(socket: &mut I2pSocket, addr: &str) -> Result<(String, String), I2pError> {
     lookup_internal(socket, &format!("NAMING LOOKUP NAME={}\n", addr))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::socket::{I2pSocket, SocketType};
+    use crate::cmd::hello::*;
+
+    #[test]
+    fn test_lookup() {
+        let mut socket = match I2pSocket::new(SocketType::Tcp, "localhost", 7656) {
+            Ok(v)  => v,
+            Err(e) => {
+                eprintln!("test_handshake: {:#?}", e);
+                assert!(false);
+                return;
+            }
+        };
+
+        // enable connection to router
+        handshake(&mut socket);
+
+        assert_eq!(
+           lookup(&mut socket, "zzz.i2p"),
+           Ok((
+               "ME".to_string(),
+               "GKapJ8koUcBj~jmQzHsTYxDg2tpfWj0xjQTzd8BhfC9c3OS5fwPBNajgF-eOD6eCjFTqTlorlh7Hnd8kXj1qblUGXT-tDoR9~YV8dmXl51cJn9MVTRrEqRWSJVXbUUz9t5Po6Xa247Vr0sJn27R4KoKP8QVj1GuH6dB3b6wTPbOamC3dkO18vkQkfZWUdRMDXk0d8AdjB0E0864nOT~J9Fpnd2pQE5uoFT6P0DqtQR2jsFvf9ME61aqLvKPPWpkgdn4z6Zkm-NJOcDz2Nv8Si7hli94E9SghMYRsdjU-knObKvxiagn84FIwcOpepxuG~kFXdD5NfsH0v6Uri3usE3uSzpWS0EHmrlfoLr5uGGd9ZHwwCIcgfOATaPRMUEQxiK9q48PS0V3EXXO4-YLT0vIfk4xO~XqZpn8~PW1kFe2mQMHd7oO89yCk-3yizRG3UyFtI7-mO~eCI6-m1spYoigStgoupnC3G85gJkqEjMm49gUjbhfWKWI-6NwTj0ZnAAAA".to_string()
+           ))
+        );
+
+        assert_eq!(
+            lookup(&mut socket, "abcdefghijklmnopqrstuvwxyz234567abcdefghijklmnopqrst.b32.i2p"),
+            Err(I2pError::InvalidValue)
+        );
+    }
+}
