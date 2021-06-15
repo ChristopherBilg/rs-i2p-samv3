d83eb4f059b7bcd7eb920c067a690296af95585f
jzm2k <jzm2k@protonmail.com>
2021-06-15 06:49:36 +0300

    cmd/hello/tests: Add tests for HELLO

--- src/cmd/hello.rs
+++ src/cmd/hello.rs
@@ -90,3 +90,68 @@ pub fn handshake(socket: &mut I2pSocket) -> Result<(), I2pError> {
     let msg = format!("HELLO VERSION MIN={} MAX={}\n", MIN_VERSION, MAX_VERSION);
     handshake_internal(socket, &msg)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::socket::{I2pSocket, SocketType};
+
+    #[test]
+    fn test_handshake() {
+        let mut socket = match I2pSocket::new(SocketType::Tcp, "localhost", 7656) {
+            Ok(v)  => v,
+            Err(e) => {
+                eprintln!("test_handshake: {:#?}", e);
+                assert!(false);
+                return;
+            }
+        };
+
+        match handshake(&mut socket) {
+            Ok(_)  => assert!(true),
+            Err(e) => {
+                eprintln!("test_handshake: {:#?}", e);
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_handshake_no_version() {
+        let mut socket = match I2pSocket::new(SocketType::Tcp, "localhost", 7656) {
+            Ok(v)  => v,
+            Err(e) => {
+                eprintln!("test_handshake: {:#?}", e);
+                assert!(false);
+                return;
+            }
+        };
+
+        assert_eq!(
+            handshake_internal(&mut socket, "HELLO VERSION"),
+            Ok(())
+        );
+    }
+
+    #[test]
+    fn test_hello_invalid_message() {
+        let mut socket = match I2pSocket::new(SocketType::Tcp, "localhost", 7656) {
+            Ok(v)  => v,
+            Err(e) => {
+                eprintln!("test_handshake: {:#?}", e);
+                assert!(false);
+                return;
+            }
+        };
+
+        assert_eq!(
+            handshake_internal(&mut socket, "HELLO test"),
+            Err(I2pError::InvalidValue)
+        );
+
+        assert_eq!(
+            handshake_internal(&mut socket, "HELLO VERSION MIN=3.1 MAX=2.8"),
+            Err(I2pError::InvalidValue)
+        );
+    }
+}
